350e10ba636f676a3da050f33f30c36f84a8acc3
EDownie <53910027+ED1978@users.noreply.github.com>
2020-01-07 15:31:50 +0000

    save method

---
+++ console.rb
@@ -0,0 +1,18 @@
+require('pry-byebug')
+require_relative('./models/Property.rb')
+
+property1 = Property.new(
+  {
+    'address' => '106 Lancefield Quay',
+    'value' => 230000,
+    'number_of_bedrooms' => 2,
+    'year_built' => 2004,
+    'buy_let_status' => 'Let',
+    'square_footage' => 250,
+    'build' => 'Flat'
+  }
+)
+property1.save()
+
+binding.pry
+nil
--- models/Property.rb
+++ models/Property.rb
@@ -2,18 +2,41 @@ require('pg')
 
 class Property
 
-attr_accessor :address, :value, :number_of_bedrooms, :year_built, :buy_let_status, :square_footage, :build
-attr_reader :id
+  attr_accessor :address, :value, :number_of_bedrooms, :year_built, :buy_let_status, :square_footage, :build
+  attr_reader :id
 
-def initialize(options)
-  @id = options['id'].to_i if options['id']
-  @address = options['address']
-  @value = options['value']
-  @number_of_bedrooms = options['number_of_bedrooms']
-  @year_built = options['year_built']
-  @buy_let_status = options['buy_let_status']
-  @square_footage = options['square_footage']
-  @build = options['build']
-end
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @address = options['address']
+    @value = options['value']
+    @number_of_bedrooms = options['number_of_bedrooms']
+    @year_built = options['year_built']
+    @buy_let_status = options['buy_let_status']
+    @square_footage = options['square_footage']
+    @build = options['build']
+  end
+
+  def save()
+    db = PG.connect({dbname: 'property_tracker', host: 'localhost'})
+    sql = "INSERT INTO properties
+    (
+      address,
+      value,
+      number_of_bedrooms,
+      year_built,
+      buy_let_status,
+      square_footage,
+      build
+    )
+    VALUES
+    (
+      $1, $2, $3, $4, $5, $6, $7
+    )
+    RETURNING *"
+    values = [@address, @value, @number_of_bedrooms, @year_built, @buy_let_status, @square_footage, @build]
+    db.prepare("save", sql)
+    @id = db.exec_prepared("save", values)[0]['id'].to_i
+    db.close
+  end
 
 end
